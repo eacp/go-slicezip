f6db78fd95bf1733cdc20bc02795615f5c1e388a
Eduardo Andrés Castillo Perera <eduardoandrescp@outlook.com>
2022-03-28 18:37:04 -0600

    Added documentation

--- zip2.go
+++ zip2.go
@@ -1,10 +1,16 @@
 package slicezip
 
+// Tuple2 is a container for 2 items. It is used for the
+// resulting slice of Zip2.
 type Tuple2[T1, T2 any] struct {
 	First  T1
 	Second T2
 }
 
+// Zip2 zips two slices of length n into an slice of n Tuple2 elements
+// where slice[i].First = s1[i] and slice[i].Second = s2[i]
+//
+// If the slices have different lengths, the shortest one will be used.
 func Zip2[T1, T2 any](s1 []T1, s2 []T2) []Tuple2[T1, T2] {
 	// Use the smallest one
 	size := len(s1)
--- zip3.go
+++ zip3.go
@@ -1,11 +1,18 @@
 package slicezip
 
+// Tuple3 is a container for 3 items. It is used for the
+// resulting slice of Zip3.
 type Tuple3[T1, T2, T3 any] struct {
 	First  T1
 	Second T2
 	Third  T3
 }
 
+// Zip3 zips two slices of length n into an slice of n Tuple3 elements
+// where slice[i].First = s1[i], slice[i].Second = s2[i]
+// and slice[i].Third = s3[i]
+//
+// If the slices have different lengths, the shortest one will be used.
 func Zip3[T1, T2, T3 any](s1 []T1, s2 []T2, s3 []T3) []Tuple3[T1, T2, T3] {
 	// Use the smallest one
 	size := len(s1)
